e55176f6f97d9500a9bded379b40622f8f8dce2a
Lu Wilson <l2wilson94@gmail.com>
2023-10-12 20:01:10 -0400

    Create styled.js

---
+++ styled.js
@@ -0,0 +1,40 @@
+<style>
+  body {
+    color: white;
+    background-color: #171d28;
+    font-family: Arial;
+  }
+
+  a {
+    color: #46ff80;
+  }
+
+  blockquote {
+    color: #9faecc;
+  }
+<style>
+<h1>Tadi web</h1>
+<p>Welcome to the tadi web. A small and simple part of the normal web.</p>
+
+<h2>Slippy mindset</h2>
+<p>The tadi web is built with a slippy mindset. We try to be as 'slippy' as possible.</p>
+<p><em>Being slippy means you're not stuck.</em></p>
+<p>When you're slippy, it's easy to change plan, or rebuild something from scratch. It means you're not locked in to using a certain tool.</p>
+<p>If something breaks, you can choose to fix it, or let it die. It's ok, because it won't take long to rebuild it from scratch.</p>
+
+<blockquote>Frogs don't live forever! They live, they make some taddies, and then they die. The taddies grow to take their place.</blockquote>
+
+<p>Every time you grow back, you'll be a bit different. You might be stronger. Or you might be better equipped for your changing needs.</p>
+
+<h2>Tadi website</h2>
+
+<p>There are many ways to make a tadi website. Here are some ideas for how you can do it.</p>
+<p>Click on an idea to see it in action!</p>
+
+<ul>
+  <li> <a href="/text.txt">Make a website with text.</a> </li>
+  <li> <a href="/">Make a website with markup.</a> </li>
+  <li> <a href="/document.html">Make a website with a document.</a> </li>
+</ul>
+
+<p>More coming soon!</p>
